94a41d4859c80fda2a63caf5a99e3ba1d132231d
RyoSci <ryo.saeki.physics@gmail.com>
2020-07-20 03:07:47 +0900

    深さ優先探索、自作コード。階層が上がるときの処理に無駄がある。

--- programming contest challenge book/dfs.py
+++ programming contest challenge book/dfs.py
@@ -7,17 +7,42 @@ k = 13
 # k = int(input())
 
 num_list = []
+has_ans = False
+ans_list = []
 def dfs(i, j):
     global num_list
-    if i == n:
-        return 0
-    # if j == 0:
+    global has_ans
+    global ans_list
+    if i == n and j == 1:
+        num_list.append(a[i-1])
+        sum_num = sum(num_list)
+        # num_list = []
+        print(sum_num)
+        return sum_num
+    elif i == n and j == 0:
+        sum_num = sum(num_list)
+        print(sum_num)
+        return sum_num
+    # if j == 0　:
     #     return 0
-    elif j == 1:
-        num_list.append(a[i])
-        return a[i]
+    if j == 1:
+        num_list.append(a[i-1])
+        
 
-    if dfs(i + 1, 0) + dfs(i + 1, 1) == k:
-        print("Yes")
+    if dfs(i + 1, 0) == k:
+        # print("Yes", num_list)
+        has_ans = True
+        ans_list = num_list[::]
+    for delete_num in range(n - i - 1):
+        num_list.pop()
+    if dfs(i + 1, 1) == k:
+        # print("Yes", num_list)
+        has_ans = True
+        ans_list = num_list[::]
 
 dfs(0, 0)
+
+if has_ans:
+    print("Yes", ans_list)
+else:
+    print("No")
