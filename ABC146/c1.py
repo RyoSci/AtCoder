21bdfbef5881c1d02ebcfc1a3bb4764efcb41ad7
RyoSci <ryo.saeki.physics@gmail.com>
2020-07-26 20:55:29 +0900

    abc 146 c別解

--- ABC146/c1.py
+++ ABC146/c1.py
@@ -9,3 +9,15 @@ for i in range(1, 19 , 1):
 res = max(0, res)
 print(min(10 ** 9, res))
 print(len("-1"))
+
+#1つのケースがどうしても通らない
+# a, b, x = map(int,input().split())
+# res = 0
+# for i in range(1, 19 , 1):
+#     tmp = (x - b * i) // a
+#     if len(str(tmp)) == i and tmp * a + len(str(tmp)) * b <= x:
+#         res = max(res, tmp)
+#         # break
+#     # print(res)
+# res = max(0, res)
+# print(min(10 ** 9, res))
