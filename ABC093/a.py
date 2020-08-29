ed183c9f16edf19a4b1f4f8a07e7b1169eb38ffe
RyoSci <ryo.saeki.physics@gmail.com>
2020-08-29 23:35:24 +0900

    変数名のif内のtypoして新たな変数を作成してWAしてしまった。

---
+++ ABC093/a.py
@@ -0,0 +1,13 @@
+s = input()
+ans = True
+if "a" not in s:
+    ans = False
+if "b" not in s:
+    ans = False
+if "c" not in s:
+    ans = False
+    
+if ans:
+    print("Yes")
+else:
+    print("No")
