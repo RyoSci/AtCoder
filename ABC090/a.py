fcdd4a26476d9795a9933045c73023e5fae66af7
RyoSci <ryo.saeki.physics@gmail.com>
2020-08-30 23:56:59 +0900

    バチャコン参加。

---
+++ ABC090/a.py
@@ -0,0 +1,6 @@
+res = ""
+for i in range(3):
+    line = input()
+    res += line[i]
+
+print(res)
---
+++ ABC090/d.py
@@ -0,0 +1,8 @@
+n, k = map(int, input().split())
+res = 0
+if k == 0:
+    print(n * n)
+else:
+    for i in range(k + 1, n + 1):
+        res += (n // i) * (i - k) + max(0, n % i - k + 1)
+    print(res)
