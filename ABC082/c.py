7431a3b2c0bb37fbcccbc45eb5668433d83ac705
RyoSci <ryo.saeki.physics@gmail.com>
2020-09-05 22:12:35 +0900

    バチャコン参加：パフォーマンス1508

---
+++ ABC082/c.py
@@ -0,0 +1,17 @@
+n = int(input())
+a = list(map(int, input().split()))
+num_map = dict()
+for i in range(n):
+    if a[i] not in num_map:
+        num_map[a[i]] = 1
+    else:
+        num_map[a[i]] += 1
+
+res = 0
+for key, value in num_map.items():
+    if key <= value:
+        res += value - key
+    else:
+        res += value
+
+print(res)
