8a05e11422a6e29bef10d8f71fc6d9111551ee82
RyoSci <ryo.saeki.physics@gmail.com>
2020-09-06 18:53:30 +0900

    バチャコン参加：パフォーマンス998

---
+++ ABC073/c.py
@@ -0,0 +1,14 @@
+n = int(input())
+paper = dict()
+for i in range(n):
+    a = int(input())
+    if a not in paper:
+        paper[a] = 1
+    else:
+        paper.pop(a)
+
+res = 0
+for value in paper.values():
+    if value == 1:
+        res += 1
+print(res)
