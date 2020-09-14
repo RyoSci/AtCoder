49a19f40830c6df620bce02da6d393ea44e99062
RyoSci <ryo.saeki.physics@gmail.com>
2020-09-14 21:49:15 +0900

    パフォーマンス：1340

---
+++ ABC061/b.py
@@ -0,0 +1,9 @@
+n, m = map(int, input().split())
+counter_list = [0] * (n + 1)
+for i in range(m):
+    a, b = map(int, input().split())
+    counter_list[a] += 1
+    counter_list[b] += 1
+
+for i in range(1, n + 1):
+    print(counter_list[i])
