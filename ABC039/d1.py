602d898e4ca8be89212329513917817fe748b0c4
RyoSci <ryo.saeki.physics@gmail.com>
2020-09-26 18:32:00 +0900

    check用の配列更新を出力用の配列の更新と同時に行った。

---
+++ ABC039/d1.py
@@ -0,0 +1,30 @@
+h, w = map(int, input().split())
+pic = [list(input()) for i in range(h)]
+res = [["." for i in range(w)] for j in range(h)]
+check = [["." for i in range(w)] for j in range(h)]
+for i in range(h):
+    for j in range(w):
+        i_u = max(i - 1, 0)
+        i_d = min(i + 1, h - 1)
+        j_l = max(j - 1, 0)
+        j_r = min(j + 1, w - 1)
+        flag = True
+        for ii in range(i_u, i_d + 1):
+            for jj in range(j_l, j_r + 1):
+                if pic[ii][jj] == ".":
+                    flag = False
+                    break
+            if not flag:
+                break
+        else:
+            res[i][j] = "#"
+            for ii in range(i_u, i_d + 1):
+                for jj in range(j_l, j_r + 1):
+                    check[ii][jj] = "#"
+
+if pic == check:
+    print("possible")
+    for i in range(h):
+        print("".join(res[i]))
+else:
+    print("impossible")
