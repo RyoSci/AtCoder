b7bb36d53300653b53c38e9480ad519277ee5c1f
RyoSci <ryo.saeki.physics@gmail.com>
2020-09-27 17:57:53 +0900

    &演算子を使うことによって1かつ１の時に１を格納する実装。

---
+++ ABC033/c2.py
@@ -0,0 +1,26 @@
+s = input() + "+0"
+n = len(s)
+res = 0
+num = int(s[0])
+for i in range(n):
+    if s[i] == "+":
+        if num == 0:
+            num = int(s[i + 1])
+            pass
+        else:
+            res += 1
+            num = int(s[i + 1])
+    elif s[i] == "*":
+        # c.py
+        # num *= int(s[i + 1])
+
+        # c1.py
+        # if int(s[i + 1]) != 0 and num != 0:
+        #     num = 1
+        # else:
+        #     num = 0
+
+        # c2.py
+        num = num > 0 and int(s[i + 1]) > 0
+
+print(res)
