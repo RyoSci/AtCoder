d0ab11e7ba1947e77a52311a2a41676ed4ee3327
RyoSci <ryo.saeki.physics@gmail.com>
2021-01-05 16:13:49 +0000

    ループと文字列操作と条件文を行う。

---
+++ Tenka1 Programmer Beginner Contest 2019/b.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <set>
+#include <string>
+#include <queue>
+#include <stack>
+using namespace std;
+#define MOD 1000000007
+#define INF (1 << 29)
+#define EPS (1e-10)
+typedef long long Int;
+typedef pair<Int, Int> P;
+
+#define max(x, y) ((x) > (y) ? (x) : (y))
+#define min(x, y) ((x) < (y) ? (x) : (y))
+
+Int n, k;
+string s, ans;
+int main()
+{
+    cin >> n >> s >> k;
+    for (char c : s)
+    {
+        if (c != s[k - 1])
+            ans += "*";
+        else
+            ans += c;
+    }
+    cout << ans << endl;
+    return 0;
+}
