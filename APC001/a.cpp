4f90ec93e6c0e0e800e1739802d64f582d696d2f
RyoSci <ryo.saeki.physics@gmail.com>
2021-02-06 14:30:51 +0000

    まずXの倍数を考えて、その数がYで割り切れなければXも割り切れないのでXの余りを調べる。

---
+++ APC001/a.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <set>
+#include <string>
+#include <queue>
+#include <stack>
+using namespace std;
+#define MOD 1000000007
+#define INF (1 << 29)
+#define EPS (1e-10)
+typedef long long Int;
+typedef pair<Int, Int> P;
+
+#define max(x, y) ((x) > (y) ? (x) : (y))
+#define min(x, y) ((x) < (y) ? (x) : (y))
+
+Int x, y, ans;
+int main()
+{
+    cin >> x >> y;
+    ans = -1;
+    if (x % y != 0)
+    {
+        ans = x;
+    }
+    cout << ans << endl;
+    return 0;
+}
