2841520a30b54b24d55f686d053a0a393766c76e
RyoSci <ryo.saeki.physics@gmail.com>
2021-02-07 14:51:56 +0000

    文字列操作。

---
+++ CODE FESTIVAL 2017 qual B/a.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <set>
+#include <string>
+#include <queue>
+#include <stack>
+using namespace std;
+#define MOD 1000000007
+#define INF (1 << 29)
+#define EPS (1e-10)
+typedef long long Int;
+typedef pair<Int, Int> P;
+
+#define max(x, y) ((x) > (y) ? (x) : (y))
+#define min(x, y) ((x) < (y) ? (x) : (y))
+
+string s, ans;
+int main()
+{
+    cin >> s;
+    for (int i = 0; i < s.length() - 8; i++)
+    {
+        ans += s[i];
+    }
+    cout << ans << endl;
+    return 0;
+}
