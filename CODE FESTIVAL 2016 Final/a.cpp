5da81b5947d353fb54cf08a7664c9a38d3b344f6
RyoSci <ryo.saeki.physics@gmail.com>
2021-02-09 15:17:02 +0000

    文字列操作。

---
+++ CODE FESTIVAL 2016 Final/a.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <set>
+#include <string>
+#include <queue>
+#include <stack>
+using namespace std;
+#define MOD 1000000007
+#define INF (1 << 29)
+#define EPS (1e-10)
+typedef long long Int;
+typedef pair<Int, Int> P;
+
+#define max(x, y) ((x) > (y) ? (x) : (y))
+#define min(x, y) ((x) < (y) ? (x) : (y))
+
+int h, w;
+string s, ans;
+int main()
+{
+    cin >> h >> w;
+    for (int i = 0; i < h; i++)
+    {
+        for (int j = 0; j < w; j++)
+        {
+            cin >> s;
+            if (s == "snuke")
+            {
+                ans = char('A' + j) + to_string(i + 1);
+            }
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
