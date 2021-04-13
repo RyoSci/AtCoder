70d568e853d329467d88281cf132f6bd12e6d2f2
RyoSci <ryo.saeki.physics@gmail.com>
2021-04-14 00:00:10 +0900

    再帰関数で再実装。

--- ARC012/b.py
+++ ARC012/b.py
@@ -1,7 +1,15 @@
 n, va, vb, l = map(int, input().split())
 
-for i in range(n):
-    time = l / va
-    l = vb * time
+# for i in range(n):
+#     time = l / va
+#     l = vb * time
 
-print(l)
+# print(l)
+
+def f(n, l):
+    if n == 0:
+        return l
+    time = l / va
+    return f(n - 1, vb * time)
+
+print(f(n, l))
