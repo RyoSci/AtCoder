86d029ad13ce5b30086b9e7427793efcad93404e
RyoSci <ryo.saeki.physics@gmail.com>
2022-03-05 07:37:58 +0000

    imos法の基礎。区間の累積を行いたいので思いつきやすい。

---
+++ 初中級者が解くべき過去問精選 100 問/081.cpp
@@ -0,0 +1,47 @@
+// #define _GLIBCXX_DEBUG
+#include <algorithm>
+// #include <atcoder/all>
+// #include <atcoder/modint>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+// using namespace atcoder;
+// using lli = long long;
+// using mint = modint1000000007;
+// using mint = modint998244353;
+#define MOD 1000000007
+#define INF (1L << 60)
+#define EPS (1e-10)
+typedef long long ll;
+typedef pair<ll, ll> P;
+#define max(x, y) ((x) > (y) ? (x) : (y))
+#define min(x, y) ((x) < (y) ? (x) : (y))
+#define rep(i, n) for (ll i = 0; i < n; i++)
+#define rep_r(i, k, n) for (ll i = k; i > n; i--)
+#define rep_s(i, k, n) for (ll i = k; i < n; i++)
+#define rep_e(e, s) for (auto e : s)
+
+int main() {
+    ll n;
+    cin >> n;
+    vector<ll> table(1000000 + 10, 0);
+    rep(i, n) {
+        ll a, b;
+        cin >> a >> b;
+        table[a]++;
+        table[b + 1]--;
+    }
+    rep(i, 1000000 + 9) { table[i + 1] += table[i]; }
+    ll ans = 0;
+    rep(i, 1000000 + 10) { ans = max(ans, table[i]); }
+    cout << ans << "\n";
+    return 0;
+}
