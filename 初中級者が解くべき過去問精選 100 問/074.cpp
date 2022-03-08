c1b83a9e6a3db71952fb8dd8d2f28f64b90dba30
RyoSci <ryo.saeki.physics@gmail.com>
2022-03-08 17:33:26 +0000

    重複組み合わせの復習。n個の玉とk個の仕切りを並べる。i番目の仕切りの左にある玉の数がaiに対応する。1<=a1より、玉１つは左端に固定して、それ以外の玉と仕切りを並び替えると式が導ける。

---
+++ 初中級者が解くべき過去問精選 100 問/074.cpp
@@ -0,0 +1,44 @@
+// #define _GLIBCXX_DEBUG
+#include <algorithm>
+#include <atcoder/all>
+#include <atcoder/modint>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+using namespace atcoder;
+using lli = long long;
+using mint = modint1000000007;
+// using mint = modint998244353;
+#define MOD 1000000007
+#define INF (1L << 60)
+#define EPS (1e-10)
+typedef long long ll;
+typedef pair<ll, ll> P;
+#define max(x, y) ((x) > (y) ? (x) : (y))
+#define min(x, y) ((x) < (y) ? (x) : (y))
+#define rep(i, n) for (ll i = 0; i < n; i++)
+#define rep_r(i, k, n) for (ll i = k; i > n; i--)
+#define rep_s(i, k, n) for (ll i = k; i < n; i++)
+#define rep_e(e, s) for (auto e : s)
+
+mint nCk(ll n, ll k) {
+    mint ans = 1;
+    rep_r(i, n, n - k) ans *= i;
+    rep_r(i, k, 0) ans /= i;
+    return ans;
+}
+
+int main() {
+    ll n, k;
+    cin >> n >> k;
+    cout << nCk(n - 1 + k, k).val() << "\n";
+    return 0;
+}
