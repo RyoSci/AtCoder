6df7b193e47ccd0a27f84273b24b49c8485ba976
佐伯凌 <ryo.saeki.physics@gmail.com>
2022-03-13 02:01:07 +0900

    テンプレ内容はスニペットで管理。

--- make_template.py
+++ make_template.py
@@ -12,12 +12,14 @@ def main():
         "import sys\n",
         "sys.setrecursionlimit(10**6)\n",
         "input = sys.stdin.readline\n",
+        "INF=10**18",
         "\n"
     ]
 
     for i in "abcdef":
         with open(os.path.join(dir_path, f"{i}.py"), "x") as f:
-            f.writelines(lines)
+            # f.writelines(lines)
+            pass
 
     with open(os.path.join(dir_path, "review.md"), "x") as f:
         for i in "abcdef":
