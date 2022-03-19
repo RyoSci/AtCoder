3fe281ed330c2d86100fd05da4f3e8036a49185a
RyoSci <ryo.saeki.physics@gmail.com>
2022-03-19 07:08:58 +0000

    解説AC。LISは知っていたが通せなかった。１次元の配列の状態は持てる。自分より手前に大きい数があるか、自分より後ろに小さい数があるか等考えたが、どこに挿入するかが決まらなかった。一旦整理するカードを一気に抜くという発想が出てこなかった。小さな制約からbit全探索で考えたら見えたかもしれない。取り出した後のカードはどこに入れても良い。

---
+++ 初中級者が解くべき過去問精選 100 問/054.cpp
@@ -0,0 +1,44 @@
+// #define _GLIBCXX_DEBUG
+#include <algorithm>
+// #include <atcoder/all>
+// #include <atcoder/modint>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+// using namespace atcoder;
+// using lli = long long;
+// using mint = modint1000000007;
+// using mint = modint998244353;
+#define MOD 1000000007
+#define INF (1L << 60)
+#define EPS (1e-10)
+typedef long long ll;
+typedef pair<ll, ll> P;
+#define max(x, y) ((x) > (y) ? (x) : (y))
+#define min(x, y) ((x) < (y) ? (x) : (y))
+#define rep(i, n) for (ll i = 0; i < n; i++)
+#define rep_r(i, k, n) for (ll i = k; i > n; i--)
+#define rep_s(i, k, n) for (ll i = k; i < n; i++)
+#define rep_e(e, s) for (auto e : s)
+
+int main() {
+    ll n;
+    cin >> n;
+    vector<ll> c(n);
+    for (ll i = 0; i < n; i++) cin >> c[i];
+    vector<ll> dp(n, INF);
+    rep(i, n) {
+        auto iter = lower_bound(dp.begin(), dp.end(), c[i]);
+        *iter = c[i];
+    }
+    cout << n - (lower_bound(dp.begin(), dp.end(), INF) - dp.begin()) << "\n";
+    return 0;
+}
